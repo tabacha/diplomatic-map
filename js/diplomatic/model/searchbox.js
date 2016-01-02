c03b225e3ce291103b96a0c7acea694212d61731
Sven Anders <github-commit@sven.anders.im>
2016-01-02 16:21:33 +0100

    Switch typeahead source, fixes #1

--- js/diplomatic/model/searchbox.js
+++ js/diplomatic/model/searchbox.js
@@ -3,56 +3,80 @@ define('diplomatic/model/searchbox', [
     'jquery',
 ],
        function (legende, $) {
-           var ele=$('#search-id');
-           ele.html('');
-           ele.append($('<option>', {id: '*'}).text('Alles'));
-           var searchGroups={};
-           for (var key in legende) {
-               var ignore=false,
-                   title=legende[key].title;
-               if (legende[key].ignore !== undefined) {
-                   ignore = legende[key].ignore;
+           function arrayToSet(a) {
+               var temp = {};
+               for (var i = 0; i < a.length; i++)
+                   temp[a[i]] = true;
+               var r = [];
+               for (var k in temp)
+                   r.push(k);
+               return r;
+           }
+           function populateTypeAhead(typeAheadStrings) {
+               var fs=$('<input type="text" id="filter-string" class="input-medium search-query" autocomplete="off">');
+               $('#filter-string').replaceWith(fs);
+               
+               var typeAheadSource=[];
+               for (var i = typeAheadStrings.length -1; i>=1; i--) {
+                   var item = typeAheadStrings[i].strip();
+                   typeAheadSource.push(item);
+                   var words = item.split(/\W+/);
+                   for (var k = words.length - 1; k >= 0; k--) {
+                       typeAheadSource.push(words[k]);
+                   }
                }
-               if (!ignore) {
-                   if (legende[key].searchGroup !== undefined) {
-                       var searchGrp=legende[key].searchGroup;
-                       if (searchGroups[searchGrp] === undefined) {
-                           ele.append($('<option>', {id: searchGrp}).text(searchGrp));
-                           searchGroups[searchGrp]= [ ];
-                       }
-                       searchGroups[searchGrp].push(key);
-                   } else {
+               typeAheadSource = arrayToSet(typeAheadSource);
+               fs.typeahead({source: typeAheadSource});
+           }
+           function create(knownTypeAhead, allTypeAhead) {
+               var ele=$('#search-id');
+               ele.html('');
+               ele.append($('<option>', {id: '*'}).text('all'));
+               for (var key in legende) {
+                   var ignore=false,
+                   title=legende[key].title;
+                   if (legende[key].ignore !== undefined) {
+                       ignore = legende[key].ignore;
+                   }
+                   if (!ignore) {
                        ele.append($('<option>', {id: key}).text(title));
                    }
                }
-           }
-           ele.change( function () {
-               $('#search-id option:selected').each(function(){
-                   var key=this.id;
-                   if (key === '*') {
-                       $('#search-op').fadeOut();
-                       $('#search-value').fadeOut();
-                       $('#filter-string').fadeIn();
-                   } else {
-                       var sKey=key;
-                       if (searchGroups[key] !== undefined) {
-                           sKey=searchGroups[key][0];
-                       }
-                       if (legende[sKey].keys === undefined) {
-                           $('#search-op').fadeIn();
+               populateTypeAhead(allTypeAhead);
+               ele.change( function () {
+
+                   $('#search-id option:selected').each(function(){
+                       var key=this.id;
+                       if (key === '*') {
+                           $('#search-op').fadeOut();
                            $('#search-value').fadeOut();
                            $('#filter-string').fadeIn();
+                           populateTypeAhead(allTypeAhead);
                        } else {
-                           $('#search-op').fadeIn();
-                           $('#search-value').fadeIn();
-                           $('#filter-string').fadeOut();
-                           var valbox=$('#search-value').html('');
-                           for (var value in legende[sKey].keys) {
-                               valbox.append($('<option>', {id: value}).text(legende[sKey].keys[value]));
+                           if (legende[key].keys === undefined) {
+                               $('#search-op').fadeIn();
+                               $('#search-value').fadeOut();
+                               $('#filter-string').fadeIn();
+                               populateTypeAhead(knownTypeAhead[key]);
+                           } else {
+                               $('#search-op').fadeIn();
+                               $('#search-value').fadeIn();
+                               $('#filter-string').fadeOut();
+                               var valbox=$('#search-value').html('');
+                               for (var value in legende[key].keys) {
+                                   valbox.append($('<option>', {id: value}).text(legende[key].keys[value]));
+                               }
+                               for (var oid in knownTypeAhead[key]) {
+
+                                   var osmkey= knownTypeAhead[key][oid];
+                                   if (legende[key].keys[osmkey] === undefined) {
+                                       valbox.append($('<option>', {id: osmkey}).text(osmkey+ ' (not documented tag)'));
+                                   }
+                               }
                            }
                        }
-                   }
+                   });
                });
-           });
-           return {'searchGroups': searchGroups};
+           };
+           return {'create': create,};
        });
