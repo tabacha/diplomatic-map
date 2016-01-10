006e1982e07f91b5140622d652b9b2e3922f928e
Sven Anders <github-commit@sven.anders.im>
2016-01-10 18:35:43 +0100

    show colored marker, depends on validation status

--- js/diplomatic/view/popup.js
+++ js/diplomatic/view/popup.js
@@ -17,30 +17,23 @@ define('diplomatic/view/popup', [
     }
     calcKeysToLower();
 
-    function click(e) {
-        var name=e.target.feature.properties.tags.name,
-            id=e.target.feature.properties.id,
-            type=e.target.feature.properties.type,
-            url=window.location.href,
-            shareTitle='diplomatic-map'+name,
-            latMin=e.target._latlng.lat-0.0015,
-            latMax=e.target._latlng.lat+0.0015,
-            lonMin=e.target._latlng.lng-0.003,
-            lonMax=e.target._latlng.lng+0.003,
-            popupj= $('<div>').addClass('popup-content'),
-            share='<div class="share">';
-
-        url=url.split('?')[0];
-        url=url.split('#')[0];
-        url=url+'?id='+id+'&type='+type;
+    function getShareLinks(url, title) {
+        share='<div class="share">';
         share+='<a href="'+url+'" title="Link zu diesem Marker"><i class="fa fa-link"></i></a>';
         url=encodeURIComponent(url);
         share+='<a href="http://www.facebook.com/sharer.php?u='+url+'&t='+ shareTitle+'" target="_blank" title="Bei Facebook teilen"><i class="fa fa-facebook"></i></a>';
-        share+='<a href="http://twitter.com/home?status='+shareTitle+' - '+url+'"  target="_blank" title="twittern"><i class="fa fa-twitter"></i></a>';        share+='<a href="mailto:?subject='+shareTitle+'&body='+url+'" title="Per E-Mail weiterleiten"><i class="fa fa-envelope"></i></a>';
+        share+='<a href="http://twitter.com/home?status='+shareTitle+' - '+url+'"  target="_blank" title="twittern"><i class="fa fa-twitter"></i></a>';
+        share+='<a href="mailto:?subject='+shareTitle+'&body='+url+'" title="Per E-Mail weiterleiten"><i class="fa fa-envelope"></i></a>';
         share+='</div>';
-        var table=$('<table class="table table-striped table-bordered table-condensed">');
-        var td=$('<td>')
-            .append($('<a>', {
+        return share;
+    } 
+    function getOSMEditorLinks(type, id, lat, lon) {
+        var latMin=lat-0.0015,
+            latMax=lat+0.0015,
+            lonMin=lon-0.003,
+            lonMax=lon+0.003,
+            div=$('<div>');
+         div.append($('<a>', {
                 href: 'https://www.openstreetmap.org/'+type+'/'+id,
                 target: '_blank',
             }).text('Show in OSM'))
@@ -66,18 +59,33 @@ define('diplomatic/view/popup', [
                 target: 'hiddenJosmIframe',
             }).text('JOSM Editor'))
         ;
-        
+        return div;
+    }
+    function getTable(target, url);
 
+        var name=target.feature.properties.tags.name,
+            id=target.feature.properties.id,
+            type=target.feature.properties.type,
+            shareTitle='diplomatic-map'+name;
+
+        url=url.split('?')[0];
+        url=url.split('#')[0];
+        url=url+'?id='+id+'&type='+type;
+        share=getShareLinks(url, shareTitle);
+        var table=$('<table class="table table-striped table-bordered table-condensed">');
+        
+        var td=$('<td>').append(
+            getOSMEditorLinks(type, id, target._latlng.lat, target._latlng.lng))
 
         table.append($('<tr>').append($('<th>').html(share)).append(td));
 
-        for (var clave in e.target.feature.properties.tags) {
+        for (var clave in target.feature.properties.tags) {
             var title = keysLowerToUpper[clave];
             if (title === undefined) {
                 console.log('undef:', clave, '.');
                 title=clave;
             }
-            var attr = e.target.feature.properties.tags[clave];
+            var attr = target.feature.properties.tags[clave];
             var ignore = false;
             var tooltip = '';
             if (legende[title] !== undefined) {
@@ -104,10 +112,10 @@ define('diplomatic/view/popup', [
                 table.append($('<tr>').html('<th title="'+tooltip+'">'+title+'</th><td>'+attr+'</td>'));
             }
         }
-        popupj.append(table);
+    function click(e) {
+        var popupj= $('<div>').addClass('popup-content'),
+        popupj.append(getTable(e.target, window.location.href));
         e.target._popup.setContent(popupj[0]);
-
-
     }
 
     return {
