a24d90a56a768dbd8fcba4b90b0b9b601ded8b93
Sven Anders <sven@anders-hamburg.de>
2016-01-18 08:26:08 +0100

    fix update problem

--- js/update-overpass.js
+++ js/update-overpass.js
@@ -1,4 +1,4 @@
-var requirejs = require('requirejs');
+var requirejs = require('requirejs'),
     navigator = {};
 requirejs(['./common'], function () {
     requirejs(['diplomatic/app/update-overpass']);
