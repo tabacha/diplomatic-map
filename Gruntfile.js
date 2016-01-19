b6ee90ab0c526472d99960bc3d9b77f41aab6c4d
Sven Anders <github-commit@sven.anders.im>
2016-01-19 16:12:00 +0100

    add basic travis-ci integration

--- Gruntfile.js
+++ Gruntfile.js
@@ -171,4 +171,5 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-require-gettext');
     grunt.task.registerTask('default', ['bower',  'git-describe', 'eslint', 'jshint', 'create_pot', 
                                         'compile_po', 'requirejs', 'cssmin', 'copy:fonts', 'copy:dist', 'copy:i18n']);
+    grunt.task.registerTask('test',[]);
 };
