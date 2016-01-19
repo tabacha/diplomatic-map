d65a8dbb55afdbb812cb2daae8ef0b9768f34d8e
Sven Anders <github-commit@sven.anders.im>
2016-01-19 15:41:29 +0100

    no i18n errors on update-overpass.js

--- js/gettext.js
+++ js/gettext.js
@@ -35,53 +35,50 @@ define('gettext', ['jed'], function () {
                     }
                 }
             } catch (e) {
-                console.error(e);
+                //console.error(e);
                 lang = 'en_US';
             }
             if (lang === undefined) {
                 lang = 'en_US';
             }
-            if (lang === 'bla') {
-                onload(function (id) {
-                    return id;
-                });
-            } else {
-                require(['i18n/'+name+'.'+lang], 
-                    function (msg) {
-                        console.log(msg);
-                        var i18n = new Jed({
-                            // This callback is called when a key is missing
-                            'missing_key_callback': function(key) {
-                                // Do something with the missing key
-                                // e.g. send key to web service or
-                                console.error('missing_i18n_key', key);
-                            },
-                            'locale_data': msg
-                        });
-                        
-                        function gt(id) {
-                            var out;
-                            if (arguments.length<2) {
-                                out=i18n.translate(id).fetch();
-                            } else {
-                                var arg=[];
-                                for (var i=1;i<arguments.length;i++) {
-                                    arg.push(arguments[i]);
-                                }
-                                console.log(arg);
-                                out=i18n.translate(id).fetch(arg);
+            require(['i18n/'+name+'.'+lang], 
+                function (msg) {
+                    var i18n = new Jed({
+                        // This callback is called when a key is missing
+                        'missing_key_callback': function(key) {
+                            // Do something with the missing key
+                            // e.g. send key to web service or
+                            if (lang!=='en_US') {
+                                console.error('missing_i18n', lang, ':', key);
                             }
-                            return out;
+                        },
+                        'locale_data': msg
+                    });
+                    
+                    function gt(id) {
+                        var out;
+                        if (arguments.length<2) {
+                            out=i18n.translate(id).fetch();
+                        } else {
+                            var arg=[];
+                            for (var i=1;i<arguments.length;i++) {
+                                arg.push(arguments[i]);
+                            }
+                            console.log(arg);
+                            out=i18n.translate(id).fetch(arg);
                         }
-                        gt.ngettext = function (singular, plural, num) {
-                            var out= i18n.translate(singular)
-                                .ifPlural( num, plural )
-                                .fetch( num );
-                            return out;
-                        };
-                        onload(gt);
-                    });        
-            }
+                        return out;
+                    }
+                    gt.ngettext = function (singular, plural, num) {
+                        var out= i18n.translate(singular)
+                            .ifPlural( num, plural )
+                            .fetch( num );
+                        return out;
+                    };
+                    gt.lang=lang;
+                    onload(gt);
+                });        
+            
         }
     };
 
