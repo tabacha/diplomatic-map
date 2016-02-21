76bf119dad902269835ff8ee38da43429bbdbc84
Sven Anders <github-commit@sven.anders.im>
2016-02-21 09:30:09 +0100

    new function findNearestCity

---
+++ js/diplomatic/model/distance_util.js
@@ -0,0 +1,26 @@
+define('diplomatic/model/distance_util', [
+    'diplomatic/model/distance',
+], function (distance) {
+
+    function findNearestCity(cities, cord, max) {
+        var skip=0;
+        var rtn=null;
+        $.each(cities, function( city, cityCord ) {
+            if ((skip===0)) {
+                var d=distance.calcFromLatLon(cord.lat,
+                                              cord.lon,
+                                              cityCord.lat, 
+                                              cityCord.lon );
+                if (d<max) {
+                    rtn=city;
+                    skip=1;
+                }
+            } // else skip
+        });
+        return rtn;
+    }
+
+    return {
+        'nearestCity': findNearestCity,
+    };
+});
